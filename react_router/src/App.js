513a73dc00dc6f8baaeb4472a6faed5d9917ec00
dougsn <douglassnascimet@gmail.com>
2022-04-26 20:11:30 -0300

    Ajeitando a dependencia do router, com o element={<Home/>}

---
+++ react_router/src/App.js
@@ -0,0 +1,11 @@
+import Routes from "./router";
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_router/src/pages/Home/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Rotas com React - Estou no Home</h1>
+    </div>
+  );
+}
