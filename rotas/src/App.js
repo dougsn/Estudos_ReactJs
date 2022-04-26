3c52b9d1221295c00e08ae7471225e14a7ab5b68
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-04-26 15:18:06 -0300

    Realizando atividades de Requisições HTTP, fetch API

--- rotas/src/App.js
+++ rotas/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    function addApi() {
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+          setNutri(json);
+        });
+    }
+
+    addApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>React Nutri</header>
+      {nutri.map((nutri) => {
+        return (
+          <article key={nutri.id} className="post">
+            <h1 className="titulo">{nutri.titulo}</h1>
+            <img src={nutri.capa} alt="Foto do blog"></img>
+            <h3>{nutri.categoria}</h3>
+            <p>{nutri.subtitulo}</p>
+            <a href="/" className="botao">Acessar</a>
+          </article>
+        );
+      })}
     </div>
   );
 }
+
+//// https://sujeitoprogramador.com/rn-api/?api=posts
 
 export default App;
